e3090dc6403ab05c6609770f4ec519beb67f2ed7
Ricardo <ricardo.almeidasantos04@gmail.com>
2025-11-13 17:05:44 -0300

    First commit initializing the project.

---
+++ src/test/java/bnr/com/ricardo/pagamentos/PagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package bnr.com.ricardo.pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
