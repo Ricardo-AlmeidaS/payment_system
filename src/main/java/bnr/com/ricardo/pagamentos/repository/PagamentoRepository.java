4f646170328c5f64b98ff85f9e3508df5b3817ad
Ricardo <ricardo.almeidasantos04@gmail.com>
2025-11-14 10:00:28 -0300

    add: service and methods

--- src/main/java/bnr/com/ricardo/pagamentos/repository/PagamentoRepository.java
+++ src/main/java/bnr/com/ricardo/pagamentos/repository/PagamentoRepository.java
@@ -2,6 +2,8 @@ package bnr.com.ricardo.pagamentos.repository;
 
 import bnr.com.ricardo.pagamentos.models.Pagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
 }
